81057bb0b3fbcc78484110c136f514eacb1ce39f
Johhan Santana <johhan.santana@gmail.com>
2020-10-20 15:25:12 -0400

    purge tailwind

--- src/pages/First.svelte
+++ src/pages/First.svelte
@@ -5,5 +5,8 @@
 <main>
   <Navigate to="/">First</Navigate>
   <Navigate to="/second">Second</Navigate>
-  <p>Hey</p>
+  <h1 class="font-plain text-3xl">hey</h1>
+  <h1 class="font-sansSemiBold text-3xl">hey</h1>
+  <h1 class="font-medium text-3xl">hey</h1>
+  <h1 class="font-bold text-3xl">hey</h1>
 </main>
